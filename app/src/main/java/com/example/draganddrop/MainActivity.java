3615df872d7c774c831b428165eda87f219af471
정광희 <kwanghee@cookmate.net>
2016-04-22 18:28:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/draganddrop/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.draganddrop;
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        startActivity(new Intent(MainActivity.this, DraggableGridExampleActivity.class));
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        fragmentTransaction.add(R.id.container, new BoardFragment(), "login");
+        fragmentTransaction.addToBackStack("login");
+        fragmentTransaction.commit();
+    }
+}
